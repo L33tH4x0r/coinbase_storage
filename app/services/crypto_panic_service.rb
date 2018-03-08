848bfe5eb2def1c39536865c2a8043dc9eb707e1
Austin <austin@dringo.io>
2018-03-07 16:22:19 -0800

    documentation: Added initial structure for service

--- app/services/crypto_panic_service.rb
+++ app/services/crypto_panic_service.rb
@@ -1,5 +1,20 @@
 class CryptoPanicService
   def call
-    
+      # Loop through api pages
+
+          # Get articles from crypopanic
+
+          # loop through results
+
+              # Store Article
+
+  end
+
+  def get_articles
+
+  end
+
+  def store_articles
+
   end
 end
