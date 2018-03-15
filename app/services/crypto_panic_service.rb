a483208545ac3e079c4eacdad512e8e0d1162b0d
austin <austin@dringo.io>
2018-03-15 11:25:20 -0700

    feature: Added parsing for data from coinbase

--- app/services/crypto_panic_service.rb
+++ app/services/crypto_panic_service.rb
@@ -1,24 +1,42 @@
 class CryptoPanicService
   def initialize
-    @token = 'e3d06307f0712ad7d0df47126a4baa446b287fb4'
+    @base_url       = "http://www.cryptopanic.com/api/posts/"
+    @auth_token     = "?auth_token=e3d06307f0712ad7d0df47126a4baa446b287fb4"
+    @base_filters   = "&public=true&currencies=BTC"
   end
 
   def call
-      # Loop through api pages
-
-          # Get articles from crypopanic
-
-          # loop through results
-
-              # Store Article
-
+    # prime loop
+    begin
+      article = HTTParty.get(@base_url+@auth_token+@base_filters).parsed_response
+    rescue
+      return nil
+    end
+    # Loop through api pages
+    while article
+      # Get next page
+      next_page = article["next"]
+      # loop through results
+      article["results"].each do |result|
+        # Store Article
+        store_articles(result)
+      end
+      # Get articles from crypopanic
+      begin
+        sleep(1)
+        article = HTTParty.get(next_page).parsed_response
+      rescue
+        return nil
+      end
+    end
+    return true
   end
 
-  def get_articles
-
-  end
-
-  def store_articles
-
+  def store_articles(result)
+    db_article = Article.create(domain: result["domain"], title: result["title"], published_at: DateTime.iso8601(result["published_at"]), identifier: result["id"])
+    db_article.votes << Vote.create(positive: result["votes"]["positive"].to_i, negative: result["votes"]["negative"], important: result["votes"]["important"])
+    result["currencies"].each do |currency|
+      db_article.currencies << Currency.create(code: currency["code"])
+    end
   end
 end
